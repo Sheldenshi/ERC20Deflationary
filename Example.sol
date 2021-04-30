f32ce11cad596c1131723177cfaafd6d6f40fabd
Shelden Shi <sheldenshi@gmail.com>
2021-04-30 16:53:47 -0700

    ExampleToken

--- Example.sol
+++ Example.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.8.4;
 
-import "./Contracts/Utils/Context.sol";
-import "./Contracts/ERC20Deflationary.sol";
-import "./Interfaces/Pancakeswap/IRouter02.sol";
+import "contracts/Utils/Context.sol";
+import "contracts/ERC20Deflationary.sol";
+import "interfaces/Pancakeswap/IRouter02.sol";
 
-contract ExampleCoin is Context, ERC20Deflationary {
+contract ExampleToken is Context, ERC20Deflationary {
 
     // pancakeswap for testnet
     address constant routerAddress = 0x73D58041eDdD468e016Cfbc13f3BDc4248cCD65D;
