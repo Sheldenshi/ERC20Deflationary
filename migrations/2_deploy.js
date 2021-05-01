22ac54b10b23c1a855122a97cd9e7a76223204a5
Shelden Shi <sheldenshi@gmail.com>
2021-04-30 17:03:23 -0700

    Update 2_deploy.js

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,6 +1,5 @@
 const ExampleToken = artifacts.require('ExampleToken');
 
 module.exports = async function (deployer) {
-  await deployer.deploy(ExampleToken,'ExampleToken', 'EXT', '100000000000');
-  const token = await ExampleToken.deployed();           
+  await deployer.deploy(ExampleToken);    
 };
