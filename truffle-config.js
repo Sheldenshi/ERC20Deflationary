745c60fbfd1dbb0613f49ee5f416534a5da55cef
Shelden Shi <sheldenshi@gmail.com>
2021-04-30 19:40:47 -0700

    downgrade @truffle/hdwallet-provider

--- truffle-config.js
+++ truffle-config.js
@@ -45,6 +45,7 @@
      testnet: {
        provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
        network_id: 97,
+       chainId: 97, 
        confirmations: 10,
        timeoutBlocks: 200,
        skipDryRun: true
@@ -52,6 +53,7 @@
      bsc: {
        provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
        network_id: 56,
+       chainId: 56, 
        confirmations: 10,
        timeoutBlocks: 200,
        skipDryRun: true
