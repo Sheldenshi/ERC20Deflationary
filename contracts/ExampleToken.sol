589600a453b6b33236e74fc4a54c151f13c9c8bc
Shelden Shi <sheldenshi@gmail.com>
2021-04-30 21:20:55 -0700

    Create ExampleToken.sol

---
+++ contracts/ExampleToken.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.8.4;
+
+import "./utils/Context.sol";
+import "./ERC20Deflationary.sol";
+
+contract ExampleToken is Context, ERC20Deflationary {
+
+    // pancakeswap for testnet
+    address routerAddress = 0xD99D1c33F9fC3444f8101754aBC46c52416550D1;
+
+    string name_ = "ExampleToken";
+    string symbol_ = "EXT";
+    uint8 decimal_ = 9;
+    uint256 totalSupply_ = 100;
+    uint8 taxFee_ = 10;
+
+    constructor () ERC20Deflationary(name_, symbol_, decimal_, totalSupply_) {
+        enableAutoBurn(taxFee_);
+        enableReward(taxFee_);
+        //enableAutoSwapAndLiquify(10, routerAddress, 10 * 10**9);
+    }
+
+}
