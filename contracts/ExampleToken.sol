760fa81bfe66c4dd33e3c5d51d43374cbc65ccb9
Shelden Shi <sheldenshi@gmail.com>
2021-05-02 14:47:49 -0700

    Update ExampleToken.sol

--- contracts/ExampleToken.sol
+++ contracts/ExampleToken.sol
@@ -10,17 +10,19 @@ contract ExampleToken is Context, ERC20Deflationary {
     string private name_ = "ExampleToken";
     string private symbol_ = "EXT";
     uint8 private decimal_ = 9;
-    uint256 private tokenSupply_ = 10 ** 15;
+    uint256 private tokenSupply_ = 10 ** 12;
     uint8 private taxBurn_ = 10;
     uint8 private taxReward_ = 10;
     uint8 private taxLiquify_ = 10;
-    uint256 private minTokensBeforeSwap_ = (10 ** 7) * (10 ** decimal_); 
-    address private pancakeswapV2Router = 0xD99D1c33F9fC3444f8101754aBC46c52416550D1;
+    uint8 private taxDecimals_ = 0;
+    uint256 private minTokensBeforeSwap_ = (10 ** 6) * (10 ** decimal_);
+    //address private pancakeswapV2Router = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
+    address private routerAddress = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
 
     constructor () ERC20Deflationary(name_, symbol_, decimal_, tokenSupply_) {
-        enableAutoBurn(taxBurn_);
-        enableReward(taxReward_);
-        enableAutoSwapAndLiquify(taxLiquify_, pancakeswapV2Router, minTokensBeforeSwap_);
+        enableAutoBurn(taxBurn_, taxDecimals_);
+        enableReward(taxReward_, taxDecimals_);
+        enableAutoSwapAndLiquify(taxLiquify_, taxDecimals_, routerAddress, minTokensBeforeSwap_);
     }
 
 }
